1442a92eac32202dd5a5f4f2e1f622607f2d4ccc
Yesenia Medina <63169653+m2-nam@users.noreply.github.com>
2021-04-17 22:32:29 -0500

    Update Rectangulo Class

--- Rectangulo.java
+++ Rectangulo.java
@@ -9,6 +9,9 @@ public class Rectangulo {
         setEsquina1(c1);
         setEsquina2(c2);
     }
+    void setR(Rectangulo R1) {
+        this.R1=R1;
+        }
     public void setEsquina1 (Coordenada coo){
         this.esquina1 = coo;
     }
@@ -20,6 +23,20 @@ public class Rectangulo {
     }
     public Coordenada getEsquina2(){
         return this.esquina2;
+    }
+    public double calArea(Coordenada c1, Coordenada c2) {
+        double area, y, x;
+        y=0;
+        x=c1.getX()-c2.getX();
+        x=c1.getY()-c2.getY();
+        if (x<0) {
+            x=x*-1;
+        }
+        if (y<0) {
+            y=y*-1;
+        }
+        area=x*y;
+        return area;
     }
     public String toString() {
         return "("+ this.esquina1 + ", " + this.esquina2 + ")";
