764f61c2c101c24b8cab5b4afbce47021cbad98c
Yesenia Medina <63169653+m2-nam@users.noreply.github.com>
2021-04-17 22:29:29 -0500

    Update Principal Class

--- Principal.java
+++ Principal.java
@@ -2,21 +2,56 @@ package src;
 
 public class Principal {
     public static void main(String[] args) {
-        Coordenada p1;
-        p1 = new Coordenada();
+        Scanner scan = new Scanner(System.in);
+        Rectangulo aRectangulo[] = new Rectangulo[20];
+        Coordenada aCoordenada[] = new Coordenada[50];
+        
+        int i;
+        double x,y;
 
-        Coordenada p2 = new Coordenada(20,30);
+        Coordenada C1 = new Coordenada(5.8,3.4);
+        Coordenada C2 = new Coordenada(1.8,0.4);
+        Rectangulo R1 = new Rectangulo(C1, C2);
+        Coordenada C3=new Coordenada(9.4,-2.5);
+        Coordenada C4=new Coordenada(4.0,4.2);
+        Rectangulo R2=new Rectangulo(C3,C4);
+        ContRectangulo Ct1=new ContRectangulo(R1,Coordenada.distancia(C1,
+        C2),R1.calculoArea(C1, C2),0);
 
-        /*System.out.println(p1);
-        p2.setX(5);
-        System.out.println(p2);
-
-        System.out.println(p2.distancia(p1));
-        System.out.println(Coordenada.distancia(p1, p2));*/
-        Rectangulo  r1 = new Rectangulo(p1,p2);
-        Rectangulo r2 = new Rectangulo (new Coordenada(-5,10), new Coordenada(3.2, -7));
-
-        System.out.println("Rectangulo r1 = "+r1);
-        System.out.println("Rectangulo r2 = "+r2);
+        System.out.println("Cantidad de rectangulos a mostrar?");
+        int n = scan.nextInt();
+        Ct1.setN(n);
+        for (i=0;i<n;i++){
+            System.out.print("Esquina 1er rectangulo: ");
+            x = scan.nextDouble();
+            y = scan.nextDouble();
+            System.out.println();
+            aCoordenada[i*4]=new Coordenada(x,y);
+            
+            System.out.print("Esquina opuesta 1er rectangulo: ");
+            x = scan.nextDouble();
+            y = scan.nextDouble();
+            System.out.println();
+            aCoordenada[(i*4)+1]=new Coordenada(x,y);
+            aCoordenada[(i*2)]=new Rectangulo(aCoordenada[i*4], aCoordenada[(i*4)+1]);
+            
+            System.out.print("Esquina 2do rectangulo:");
+            x = scan.nextDouble();
+            y = scan.nextDouble();
+            System.out.println();
+            aCoordenada[(i*4)+2]=new Coordenada(x,y);
+            
+            System.out.print("Esquina opuesta 2do rectangulo:");
+            x = scan.nextDouble();
+            y = scan.nextDouble();
+            System.out.println();
+            aCoordenada[(i*4)+3]=new Coordenada(x,y);
+            aCoordenada[(i*2)+1]=new Rectangulo(aCoordenada[(i*4)+2], aCoordenada[(i*4)+3]);
+            
+            System.out.println("Rectangulo A="+aRectangulo[i*2].toString());
+            System.out.println("Rectangulo B="+aRectangulo[(i*2)+1].toString());
+            Verificador V1=new Verificador(aRectangulo[i*2],aRectangulo[(i*2)+1]);
+            V1.Verific(aRectangulo[i*2], aRectangulo[(i*2)+1]);
+        }
     }
 }
